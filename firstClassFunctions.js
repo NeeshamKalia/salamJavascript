ad1935c2d4352b8d02dd67f6ee0f56bf1dc812a6
NeeshamKalia <neeshamkalia007@gmail.com>
2022-11-19 12:39:11 +0530

    highOrderfunctions

---
+++ firstClassFunctions.js
@@ -0,0 +1,18 @@
+// Function statement aka Function declaration
+a() //
+b()  // b is not a function error
+ function a(){
+    console.log("a called")
+ }
+
+ // Function Expression
+
+ var b = function(){
+    console.log('b called')
+ }
+
+ //Anonymous function
+
+ let c =  () => {
+
+ }
